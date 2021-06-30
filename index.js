f6e8ec83b2897d97eba1996d5242544640158d5b
harshuuu18 <probookh118@gmail.com>
2021-06-30 07:39:47 +0530

    main

--- index.js
+++ index.js
@@ -77,14 +77,14 @@ app.post('/update', async (req,res)=>{
     
 })
 
-// if(process.env.NODE_ENV == 'production'){
-//     const path = require('path')
+if(process.env.NODE_ENV == 'production'){
+    const path = require('path')
 
-//     app.get('/',(req,res)=>{
-//         app.use(express.static(path.resolve(__dirname,'client','build')))
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(port,()=>{
     console.log("server is running on port");
